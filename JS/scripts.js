fef91f18e8897933759c07eea547e91f26861fa4
netovieira01_ <netinhovieira1801@gmail.com>
2022-08-11 14:08:55 -0300

    Variáveis Var Let e Const

--- JS/scripts.js
+++
@@ -1,9 +0,0 @@
-// aqui criamos um Object copo
-const copo = {
-  material: "vidro", //atributo String
-  capacidade: 250, // atributo Number
-  cheio: function () {
-
-  }
-}
-  // aqui podemos declarar uma função/método que será atribuído ao Object
