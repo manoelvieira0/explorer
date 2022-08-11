212e803335aa4a43a5345a178bb5a710d365bd85
netovieira01_ <netinhovieira1801@gmail.com>
2022-08-11 13:51:10 -0300

    objetos

--- JS/strings.js
+++
@@ -1,7 +0,0 @@
-' ' // aspas simples
-" " // aspas duplas
-` ` // template literals, para incluir outras variáveis e expressões d
-
-console.log(`A soma de duas unidades é ${1+1}`)
-
-console.log("Isso é um texto")
