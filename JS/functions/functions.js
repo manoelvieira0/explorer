6d2fe57bbb7822608a0f4f76c1cc3db1392932fd
netovieira01_ <netinhovieira1801@gmail.com>
2022-08-12 08:50:03 -0300

    Outra maneira de entender funções
    Uma outra forma de entender funções por meio de um exemplo de liquidificador.

--- JS/functions/functions.js
+++ JS/functions/functions.js
@@ -1,15 +1,7 @@
-// function expression
-// function anonymous
+// arrow function
 
-// parameters
-const sum = function(number1, number2){
-  let total = number1 + number2
-  return total
+const sayMyName = () => {
+  console.log('Neto')
 }
 
-let number1 = 34
-let number2 = 25
-
-console.log(`o número 1 é ${number1}`)
-console.log(`o número 1 é ${number2}`)
-console.log(`a soma é ${sum(number1, number2)}`)
+sayMyName()
--- JS/functions/functions1.js
+++ JS/functions/functions1.js
@@ -1,9 +1,11 @@
 // Criar app de frases motivacionais
 
+// Declaração da função
 function createPhrases(){
   console.log('Estudar é muito bom')
   console.log('Paciência e persistência')
   console.log('Revisão é a mãe do aprendizado')
 }
 
+// Executar a função
 createPhrases()
