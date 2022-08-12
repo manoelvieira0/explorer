58af3786f0cd382c8694d8679141a8d9ccdc08a0
netovieira01_ <netinhovieira1801@gmail.com>
2022-08-12 17:24:40 -0300

    Update script.js
    - Quais os dados de entrada que terei? (inputs)
    - Quais as variáveis?
    - Qual o caminho ideal? (fluxo ideal)
    - Qual(is) o(s) caminho(s) alternativo(s)? (fluxo alternativo)
    - Quais tratamentos de dados preciso fazer? (estrutura de dados, funções)
    - Qual é a saída, ou quais os dados de saída, esperada? (outputs)
    
    Ferramentas da linguagem que abordaremos
    [] loop while
    [] arrays e funções de arrays
    [] condicional if/else
    [] template literals (strings)

--- JS/script.js
+++ JS/script.js
@@ -25,30 +25,37 @@ let items = []
 while (option != 3) {
 
   option = Number(prompt(`
-  Olá Usuário! Digite o número da opção desejada:
+  Olá usuário! Digite o número da opção desejada
   
-    1. Cadastrar um item na lista
-    2. Mostrar itens cadastrados
-    3. Sair do programa
-  `))
+  1. Cadastrar um item na lista
+  2. Mostrar itens cadastrados
+  3. Sair do programa
+`))
 
-  if (option == 1) {
-    items[0] = prompt('Digite o nome do item: ')
+  switch (option) {
+    case 1:
+      let item = prompt("Digite o nome do item")
+      items.push(item)
+      break;
+    case 2:
+      if (items.length == 0) {
+        alert("Não existem itens cadastrados")
+      } else {
+        alert(items)
+      }
+      break;
+    case 3:
+      alert('Tchau')
+      break;
+    default:
+      alert("Opção Inválida, Tente Novamente!")
   }
-
-  else if (option == 2) {
-
-    if (items.length == 0) {
-      alert('Não existem itens cadastrados')
-    } else {
-      alert(items)
-    }
-
-  } else {
-    alert('Tchau')
-  }
-
-  console.log(items)
-  console.log(option)
 }
+
+
+
+
+
+
+
 
