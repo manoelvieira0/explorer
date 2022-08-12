42fb2f8b3c38e199729f7e7e51e2781b9aa38dc6
netovieira01_ <netinhovieira1801@gmail.com>
2022-08-12 08:35:10 -0300

    Retornando valores dentro da função
    Para passar valores de dentro da função para o resto do código, usa-se a palavra reservada return seguida do valor à ser retornado, parando assim a execução da função e mandando o valor para onde a função foi chamada.
    
    Tópicos relacionados à esta aula

--- JS/functions/functions.js
+++ JS/functions/functions.js
@@ -3,8 +3,13 @@
 
 // parameters
 const sum = function(number1, number2){
-  console.log(number1 + number2)
+  let total = number1 + number2
+  return total
 }
 
-sum(2, 3) //arguments
+let number1 = 34
+let number2 = 25
 
+console.log(`o número 1 é ${number1}`)
+console.log(`o número 1 é ${number2}`)
+console.log(`a soma é ${sum(number1, number2)}`)
