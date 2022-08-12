76fa9b8a7e8528001a1c826e88887d17f7428d19
netovieira01_ <netinhovieira1801@gmail.com>
2022-08-12 16:19:39 -0300

    Calculando a média de um estudante
    # Fluxos da aplicação
    
    - Caminhos lógicos baseado em algum dado ou informação
    
    # Boas práticas na programação
    
    - Não repetir código
    
    [] Tipo de dado: Boolean (true | false)
    [] if/else (elseif)
    [] Operadores comparativos
    
    (maior que)
    < (menor que)
    = (maior ou igual a)
    <= (menor ou igual a)
    == (igual a)
    != (diferente de)

---
+++ JS/03/notes.md
@@ -0,0 +1,6 @@
+# Melhorar lógica de programação
+
+- Perguntas corretas
+- Entender Problemas
+
+Operadores matemáticos
--- JS/script.js
+++ JS/script.js
@@ -1,26 +1,37 @@
 /*
-  Capturar 2 números
-  e fazer as operações matemáticas
-  de soma, subtração, multiplicação,
-  divisão e resto da divisão.
+  Solicitar o nome do aluno e as 3 notas
+  do bimestre calcular a média daquele aluno.
 
-  E para cada operação, mostrar um alerta
-  com o resultado.
+  A média positiva deverá ser maior que 6
+
+  Se o aluno passou no bimestre, dar os 
+  parabéns.
+
+  Se o aluno não passou no bimestre, 
+  motivar o aluno a dar seu melhor na prova
+  de recuperação.
+
+  Em ambos os casos, mostre uma mensagem com o nome do aluno e a nota
 */
 
-alert('Informe dois números: ')
-let numberOne = prompt('Informe o primeiro número: ')
-let numberTwo = prompt('Informe o segundo número: ')
+let student = prompt('Informe o nome do(a) Aluno(a): ')
+let n1 = prompt('Qual a nota da primeira prova: ')
+let n2 = prompt('Qual a nota da segunda prova: ')
+let n3 = prompt('Qual a nota da terceira prova: ')
 
-numberOne = Number(numberOne)
-numberTwo = Number(numberTwo)
+n1 = Number(n1)
+n2 = Number(n2)
+n3 = Number(n3)
 
-const sum = numberOne + numberTwo
-const sub = numberOne - numberTwo
-const mul = numberOne * numberTwo
-const div = numberOne / numberTwo
+let media = (n1 + n2 + n3) / 3
 
-alert('Soma: ' + sum)
-alert('Subtração: ' + sub)
-alert('Multiplicação' + mul)
-alert('Divisão: ' + div)
+let result = media >= 6
+
+media = media.toFixed(1)
+
+if(result) {
+  alert('Parabéns '+ student +' você passou! Sua média foi de: '+ media)
+} else if(media < 6){
+  alert(student + ', não foi dessa vez, estude para a prova de recuperação! Sua média foi de: '+ media)
+}
+
