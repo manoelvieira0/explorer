c682eece3652eace56b22a18e6cc3d4de7b16630
netovieira01_ <netinhovieira1801@gmail.com>
2022-08-16 09:51:14 -0300

    Aplicando a lógica

--- jogo-adivinhacao/main.js
+++ jogo-adivinhacao/main.js
@@ -1,7 +1,19 @@
+const randomNumber = Math.round(Math.random() * 10)
+let attempts = 1
+
 function handleClick(event){
   event.preventDefault()
 
   const inputNumber = document.querySelector("#inputNumber")
   
-  console.log(inputNumber.value)
+  if(Number(inputNumber.value) ==  randomNumber) {
+    document.querySelector('.screen1').classList.add('hide')
+    document.querySelector('.screen2').classList.remove('hide')
+
+    document.querySelector('.screen2 h2').innerText = 
+    `Você acertou em ${attempts} tentativas`
+  }
+
+  attempts++
+
 }
