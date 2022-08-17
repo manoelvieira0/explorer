a419e04ccd27d2e43a91aece321a6da1f6942976
netovieira01_ <netinhovieira1801@gmail.com>
2022-08-17 08:58:19 -0300

    Apresentando o resultado do IMC no popup

--- IMC/script.js
+++ IMC/script.js
@@ -3,11 +3,29 @@ const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
-form.onsubmit = function(event){
+const modalWrapper = document.querySelector('.modal-wrapper')
+const modalMessage = document.querySelector('.modal .title span')
+const modalBtnClose = document.querySelector('.modal button.close')
+
+form.onsubmit = function (event) {
   event.preventDefault()
-  
+
   const weight = inputWeight.value
   const height = inputHeight.value
 
-  console.log(weight, height)
+  const result = IMC(weight, height)
+  const message = `Seu IMC é de ${result}`
+
+  modalWrapper.classList.add('open')
+  modalMessage.innerText = message
+}
+
+modalBtnClose.onclick = () => {
+  modalWrapper.classList.remove('open')
+  inputHeight = ''
+  inputWeight = ''
+}
+
+function IMC(weight, height) {
+  return (weight / ((height / 100) ** 2)).toFixed(2)
 }
