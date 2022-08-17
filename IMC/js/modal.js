d9bd1a90086af3557e8a29ba5c01e3959dffaf04
netovieira01_ <netinhovieira1801@gmail.com>
2022-08-17 11:35:10 -0300

    Modularizando o Modal popup

---
+++ IMC/js/modal.js
@@ -0,0 +1,15 @@
+export const Modal = {
+  whapper: document.querySelector('.modal-wrapper'),
+  message: document.querySelector('.modal .title span'),
+  buttonClose: document.querySelector('.modal button.close'),
+  open() {
+    Modal.whapper.classList.add('open')
+  },
+  close() {
+    Modal.whapper.classList.remove('open')
+  }
+}
+
+Modal.buttonClose.onclick = () => {
+  Modal.close()
+}
--- IMC/js/script.js
+++ IMC/js/script.js
@@ -1,19 +1,9 @@
+import { Modal } from './modal.js'
+
 // Variáveis
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
-
-const Modal = {
-  whapper: document.querySelector('.modal-wrapper'),
-  message: document.querySelector('.modal .title span'),
-  buttonClose: document.querySelector('.modal button.close'),
-  open() {
-    Modal.whapper.classList.add('open')
-  },
-  close() {
-    Modal.whapper.classList.remove('open')
-  }
-}
 
 form.onsubmit = function (event) {
   event.preventDefault()
@@ -26,10 +16,6 @@ form.onsubmit = function (event) {
 
   Modal.message.innerText = message
   Modal.open()
-}
-
-Modal.buttonClose.onclick = () => {
-  Modal.close()
 }
 
 function IMC(weight, height) {
