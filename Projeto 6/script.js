6e7ceca669ea321007de303ebe6e97e43658fecb
netovieira01_ <netinhovieira1801@gmail.com>
2022-08-17 13:55:24 -0300

    Refatorando para as novas funcionalidades

--- Projeto 6/script.js
+++ Projeto 6/script.js
@@ -1,17 +1,39 @@
 // DOM
 // Document Object Model
-
-let play = document.querySelector('.play')
-let pause = document.querySelector('.pause')
+const buttonPlay = document.querySelector('.play')
+const buttonPause = document.querySelector('.pause')
+const buttonStop = document.querySelector('.stop')
+const buttonSet = document.querySelector('.set')
+const buttonSoundOn = document.querySelector('.sound-on')
+const buttonSoundOff = document.querySelector('.sound-off')
 // Event-driven
 // programação imperativa
 // callback
-play.addEventListener('click', function() {
-  play.classList.add('hide')
-  pause.classList.remove('hide')
+buttonPlay.addEventListener('click', function() {
+  buttonPlay.classList.add('hide')
+  buttonPause.classList.remove('hide')
+  buttonSet.classList.add('hide')
+  buttonStop.classList.remove('hide')
 })
 
-pause.addEventListener('click', function() {
-  pause.classList.add('hide')
-  play.classList.remove('hide')
+buttonPause.addEventListener('click', function() {
+  buttonPause.classList.add('hide')
+  buttonPlay.classList.remove('hide')
+})
+
+buttonStop.addEventListener('click', function(){
+  buttonPlay.classList.remove('hide')
+  buttonPause.classList.add('hide')
+  buttonSet.classList.remove('hide')
+  buttonStop.classList.add('hide')
+})
+
+buttonSoundOff.addEventListener('click', function(){
+  buttonSoundOn.classList.remove('hide')
+  buttonSoundOff.classList.add('hide')
+})
+
+buttonSoundOn.addEventListener('click', function(){
+  buttonSoundOn.classList.add('hide')
+  buttonSoundOff.classList.remove('hide')
 })
