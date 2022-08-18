d322afc692058cf694d33d0e90ccae4dd3f6588e
netovieira01_ <netinhovieira1801@gmail.com>
2022-08-18 15:38:50 -0300

    Avançando na lógica com pequenos passos

--- Projeto 6/script.js
+++ Projeto 6/script.js
@@ -4,22 +4,35 @@ const buttonStop = document.querySelector('.stop')
 const buttonSet = document.querySelector('.set')
 const buttonSoundOn = document.querySelector('.sound-on')
 const buttonSoundOff = document.querySelector('.sound-off')
-let minutes 
+let minutes
 const minutesDisplay = document.querySelector('.minutes')
 const secondsDisplay = document.querySelector('.seconds')
 
-function countDown(){
-  setTimeout(function(){
-    let seconds = Number(secondsDisplay.textContent) 
-    if(seconds <= 0){
-      seconds = 60
+function countDown() {
+  setTimeout(function () {
+    let seconds = Number(secondsDisplay.textContent)
+    let minutes = Number(minutesDisplay.textContent)
+
+    if (minutes <= 0) {
+      buttonPlay.classList.remove('hide')
+      buttonPause.classList.add('hide')
+      buttonSet.classList.remove('hide')
+      buttonStop.classList.add('hide')
+      return
     }
-    secondsDisplay.textContent = seconds - 1
+
+    if (seconds <= 0) {
+      seconds = 2
+      minutesDisplay.textContent = String(minutes - 1).padStart(2, '0')
+    }
+
+    secondsDisplay.textContent = String(seconds - 1).padStart(2, '0')
+
     countDown()
   }, 1000)
 }
 
-buttonPlay.addEventListener('click', function() {
+buttonPlay.addEventListener('click', function () {
   buttonPlay.classList.add('hide')
   buttonPause.classList.remove('hide')
   buttonSet.classList.add('hide')
@@ -28,29 +41,29 @@ buttonPlay.addEventListener('click', function() {
   countDown()
 })
 
-buttonPause.addEventListener('click', function() {
+buttonPause.addEventListener('click', function () {
   buttonPause.classList.add('hide')
   buttonPlay.classList.remove('hide')
 })
 
-buttonStop.addEventListener('click', function(){
+buttonStop.addEventListener('click', function () {
   buttonPlay.classList.remove('hide')
   buttonPause.classList.add('hide')
   buttonSet.classList.remove('hide')
   buttonStop.classList.add('hide')
 })
 
-buttonSoundOff.addEventListener('click', function(){
+buttonSoundOff.addEventListener('click', function () {
   buttonSoundOn.classList.remove('hide')
   buttonSoundOff.classList.add('hide')
 })
 
-buttonSoundOn.addEventListener('click', function(){
+buttonSoundOn.addEventListener('click', function () {
   buttonSoundOn.classList.add('hide')
   buttonSoundOff.classList.remove('hide')
 })
 
-buttonSet.addEventListener('click', function(){
+buttonSet.addEventListener('click', function () {
   minutes = prompt('Quantos Minutos?')
-  minutesDisplay.textContent = minutes
+  minutesDisplay.textContent = String(minutes).padStart(2, '0')
 })
