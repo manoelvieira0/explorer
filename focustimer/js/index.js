f934534f27f8a0a4711a5b876e2aefd81a434a77
netovieira01_ <netinhovieira1801@gmail.com>
2022-09-01 09:01:45 -0300

    Alterando comportamento padrão do evento de redirecionamento

---
+++ focustimer/js/index.js
@@ -0,0 +1,8 @@
+const buttonPlay = document.querySelector('.play')
+const buttonStop = document.querySelector('.stop')
+const buttonUp = document.querySelector('.up')
+const buttonDown = document.querySelector('.down')
+
+buttonPlay.addEventListener('click', function(){
+  
+})
