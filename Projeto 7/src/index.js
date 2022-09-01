da2805d1ac396a25edc64ed9b5c16a39832dfc11
netovieira01_ <netinhovieira1801@gmail.com>
2022-09-01 10:28:49 -0300

    Orientação a Objetos no JavaScript

--- Projeto 7/src/index.js
+++ Projeto 7/src/index.js
@@ -1,29 +1,13 @@
-const routes = {
-  "/": "/pages/home.html",
-  "/about": "/pages/about.html",
-  "/contact": "/pages/contact.html",
-  404: "/pages/404.html",
-}
+import './router.js'
 
-function route(event) {
-  event = event || window.event
-  event.preventDefault()
-
-  window.history.pushState({}, "", event.target.href)
-
-  handle()
-}
-
-function handle() {
-  const { pathname } = window.location
-  const route = routes[pathname] || routes[404]
-
-  fetch(route)
-    .then(data => data.text())
-    .then(html => {
-      document.querySelector('#app').innerHTML = html
-    })
-}
-handle()
-
-window.onpopstate = () => handle()
+// const routes = {
+//   "/": "/pages/home.html",
+//   "/about": "/pages/about.html",
+//   "/contact": "/pages/contact.html",
+//   404: "/pages/404.html",
+// }
+// 
+// 
+// handle()
+// 
+// window.onpopstate = () => handle()
