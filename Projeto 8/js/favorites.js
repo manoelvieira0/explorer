327a12cbc4eff69047c34da06dbe2e3f2bf2d137
netovieira01_ <netinhovieira1801@gmail.com>
2022-09-02 09:28:49 -0300

    ajustes finais

--- Projeto 8/js/favorites.js
+++ Projeto 8/js/favorites.js
@@ -1,24 +1,8 @@
-export class GitHubUser {
-  static search(username) {
-    const endpoint = `https://api.github.com/users/${username}`
-
-    return fetch(endpoint)
-      .then(data => data.json())
-      .then(data => ({
-        login: data.login,
-        dame: data.name,
-        public_repos: data.public_repos,
-        followers: data.followers
-      }))
-  }
-}
+import { GitHubUser } from "./GitHubUser.js"
 
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root)
-
-    this.tbody = this.root.querySelector('table tbody')
-
     this.load()
   }
 
@@ -28,7 +12,17 @@ export class Favorites {
 
   async add(username) {
     try {
+
+      const userExists = this.entries.find(entry => entry.login === username)
+
+      console.log(userExists)
+
+      if(userExists) {
+        throw new Error('Usuário já cadastrado')
+      }
+
       const user = await GitHubUser.search(username)
+      console.log(user.name)
       
       if(user.login === undefined){
         throw new Error('Usuário não encontrado!')
@@ -61,6 +55,8 @@ export class FavoritesView extends Favorites {
   constructor(root) {
     super(root)
 
+    this.tbody = this.root.querySelector('table tbody')
+
     this.update()
     this.onAdd()
   }
@@ -82,6 +78,7 @@ export class FavoritesView extends Favorites {
       row.querySelector('.user img').src = `https://github.com/${user.login}.png`
       row.querySelector('.user img').alt = `Imagem de ${user.name}`
       row.querySelector('.user p').textContent = user.name
+      row.querySelector('.user a').href = `https://github.com/${user.login}`
       row.querySelector('.user span').textContent = user.login
       row.querySelector('.repositories').textContent = user.public_repos
       row.querySelector('.followers').textContent = user.followers
