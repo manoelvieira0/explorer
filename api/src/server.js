f60e02dd88bdd37b2926d144f23beb2ab8b32126
netovieira01_ <netinhovieira1801@gmail.com>
2022-09-06 08:35:30 -0300

    Método POST

--- api/src/server.js
+++ api/src/server.js
@@ -3,17 +3,8 @@ const express = require('express')
 
 const app = express()
 
-app.get("/message/:id/:user", (request, response) => {
-  const { id, user } = request.params
-
-  response.send(`Mensagem ID: ${id}. 
-  Para o usuário: ${user}`)
-})
-
-app.get("/users", (request, response) => {
-  const {page, limit} = request.query
-
-  response.send(`Página: ${page} e Limite: ${limit}`)
+app.post("/users", (request, response) => {
+  response.send("Você chamou o post!")
 })
 
 const PORT = 3333
