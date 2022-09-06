4d0c4f317557d6c623a047161cc5e4d9ec59b69f
netovieira01_ <netinhovieira1801@gmail.com>
2022-09-06 13:24:05 -0300

    App Error

--- api/src/routes/users.routes.js
+++ api/src/routes/users.routes.js
@@ -4,17 +4,8 @@ const UsersController = require('../controllers/UsersController')
 
 const usersRoutes = Router()
 
-function myMiddleware(request, response, next){
-  console.log("Você passou pelo Middleware!")
-  if(!request.body.isAdmin){
-    return response.json({message: "user unauthorized"})
-  }
-
-  next()
-}
-
 const usersController = new UsersController()
 
-usersRoutes.post("/", myMiddleware, usersController.create)
+usersRoutes.post("/", usersController.create)
 
 module.exports = usersRoutes
