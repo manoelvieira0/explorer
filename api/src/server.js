2306600a99951bb9b018ad7aca111f707fb6b937
netovieira01_ <netinhovieira1801@gmail.com>
2022-09-08 19:24:53 -0300

    Migrations

--- api/src/server.js
+++ api/src/server.js
@@ -1,20 +1,20 @@
 require("express-async-errors")
-const database = require("./database/sqlite")
+const migrationsRun = require("./database/sqlite/migrations")
 
 const AppError = require("./utils/AppError")
 const express = require('express')
 
 const routes = require("./routes")
 
+migrationsRun()
+
 const app = express()
 app.use(express.json())
 
 app.use(routes)
 
-database()
-
 app.use((error, request, response, next) => {
-  if(error instanceof AppError){
+  if (error instanceof AppError) {
     return response.status(error.statusCode).json({
       status: "error",
       message: error.message
