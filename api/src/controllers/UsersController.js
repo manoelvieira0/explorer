af6c9545da0a40967c645eca81bfc982ed477b0d
netovieira01_ <netinhovieira1801@gmail.com>
2022-09-08 19:38:15 -0300

    Cadastrando usuário

--- api/src/controllers/UsersController.js
+++ api/src/controllers/UsersController.js
@@ -8,9 +8,11 @@ class UsersController {
 
     const database = await sqliteConnection()
     const checkUserExist = await database.get('SELECT * FROM users WHERE email = (?)', [email])
-    if(checkUserExist){
+    if (checkUserExist) {
       throw new AppError('Este e-mail já está em uso.')
     }
+
+    await database.run("INSERT INTO users(name, email, password) VALUES (?, ?, ?)", [name, email, password]);
 
     return response.status(201).json()
 
