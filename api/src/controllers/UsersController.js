d813cfab8f10483a6341552f94a560ccf622e19b
netovieira01_ <netinhovieira1801@gmail.com>
2022-09-11 08:14:12 -0300

    Atualizando usuário
    lógica para que os dados dos usuários sejam atualizados.

--- api/src/controllers/UsersController.js
+++ api/src/controllers/UsersController.js
@@ -21,6 +21,38 @@ class UserController {
 
     return response.status(201).json()
   }
+
+  async update(request, response) {
+    const { name, email } = request.body
+    const { id } = request.params
+
+    const database = await sqliteConnection()
+    const user = await database.get('SELECT * FROM users WHERE id = (?)', [id])
+
+    if (!user) {
+      throw new AppError('Usuário não encontrado.')
+    }
+
+    const userWithUptadatedEmail = await database.get('SELECT * FROM users WHERE email = (?)', [email])
+
+    if (userWithUptadatedEmail && userWithUptadatedEmail.id !== user.id) {
+      throw new AppError('Este e-mail já está em uso')
+    }
+
+    user.name = name
+    user.email = email
+
+    await database.run(
+      `UPDATE users SET 
+      name = ?, 
+      email = ?, 
+      updated_at = ? 
+      WHERE ID = ?`,
+      [user.name, user.email, new Date(), id]
+    );
+
+    return response.json()
+  }
 }
 
 module.exports = UserController
--- api/src/routes/users.routes.js
+++ api/src/routes/users.routes.js
@@ -7,5 +7,6 @@ const usersController = new UserController()
 const usersRouter = Router()
 
 usersRouter.post("/", usersController.create)
+usersRouter.put("/:id", usersController.update)
 
 module.exports = usersRouter
