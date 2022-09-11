daf5a378ff91bd06700d53b7a08b5344d01b34b7
netovieira01_ <netinhovieira1801@gmail.com>
2022-09-11 07:44:02 -0300

    Cadastrando usuário
    cadastrar usuários no banco de dados através do nosso Controller.

--- api/src/controllers/UsersController.js
+++ api/src/controllers/UsersController.js
@@ -10,9 +10,12 @@ class UserController {
 
     const checkUserExist = await database.get('SELECT * FROM users WHERE email = (?)', [email])
 
-    if(checkUserExist){
+    if (checkUserExist) {
       throw new AppError('Este e-mail já está em uso.')
     }
+
+    await database.run('INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
+      [name, email, password]);
 
     return response.status(201).json()
   }
