724b56d5db0cea4cedc87994fb26c51a819e21cb
netovieira01_ <netinhovieira1801@gmail.com>
2022-09-12 13:21:01 -0300

    Filtro WhereIn

--- api/src/controllers/NotesController.js
+++ api/src/controllers/NotesController.js
@@ -50,14 +50,25 @@ class NotesController {
     await knex('notes').where({ id }).delete()
 
     return response.json()
-  } 
+  }
 
   async index(request, response) {
-    const {title, user_id} = request.query
-    const notes = await knex('notes').where({user_id}).whereLike("title", `%${title}%`).orderBy("title")
+    const { title, user_id, tags } = request.query
+
+    let notes
+
+    if (tags) {
+      const filterTags = tags.split(',').map(tag => tag.trim());
+      notes = await knex('tags').whereIn('name', filterTags);
+    } else {
+      notes = await knex('notes')
+        .where({ user_id })
+        .whereLike("title", `%${title}%`)
+        .orderBy("title")
+    }
 
     return response.json(notes)
-  } 
+  }
 }
 
 module.exports = NotesController
