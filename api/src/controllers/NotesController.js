fc45be4f08f55bd2e3742fd9454b314e9ef101a0
netovieira01_ <netinhovieira1801@gmail.com>
2022-09-12 11:15:21 -0300

    Deletando nota

--- api/src/controllers/NotesController.js
+++ api/src/controllers/NotesController.js
@@ -35,13 +35,21 @@ class NotesController {
     const { id } = request.params
     const note = await knex('notes').where({ id }).first()
     const tags = await knex('tags').where({ note_id: id }).orderBy("name")
-    const links = await knex('links').where({note_id: id}).orderBy('created_at')
+    const links = await knex('links').where({ note_id: id }).orderBy('created_at')
 
     return response.json({
       ...note,
       tags,
       links
     })
+  }
+
+  async delete(request, response) {
+    const { id } = request.params
+
+    await knex('notes').where({ id }).delete()
+
+    return response.json()
   }
 }
 
