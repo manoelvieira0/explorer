a80c4f2341842845664482604f79c416dd2aace4
netovieira01_ <netinhovieira1801@gmail.com>
2022-09-12 11:24:04 -0300

    Operador Like
    like para buscar valores que contenham partes de uma palavra.

--- api/src/controllers/NotesController.js
+++ api/src/controllers/NotesController.js
@@ -53,8 +53,8 @@ class NotesController {
   } 
 
   async index(request, response) {
-    const {user_id} = request.query
-    const notes = await knex('notes').where({user_id}).orderBy("title")
+    const {title, user_id} = request.query
+    const notes = await knex('notes').where({user_id}).whereLike("title", `%${title}%`).orderBy("title")
 
     return response.json(notes)
   } 
