4ffa7c47dc16d330a1eeeda4327fe85657192767
netovieira01_ <netinhovieira1801@gmail.com>
2022-09-12 10:58:08 -0300

    Cadastrando nota

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,8 +1,10 @@
 const {Router} = require('express')
 
 const usersRouter = require('./users.routes')
+const notesRoutes = require('./notes.routes')
 
 const routes = Router()
 routes.use("/users", usersRouter)
+routes.use("/notes", notesRoutes)
 
 module.exports = routes
