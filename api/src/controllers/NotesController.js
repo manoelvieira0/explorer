6faf234c7fbd48c2b5ec56aa6ab8af3b74a049d2
netovieira01_ <netinhovieira1801@gmail.com>
2022-09-12 11:19:41 -0300

    Listando notas

--- api/src/controllers/NotesController.js
+++ api/src/controllers/NotesController.js
@@ -50,7 +50,14 @@ class NotesController {
     await knex('notes').where({ id }).delete()
 
     return response.json()
-  }
+  } 
+
+  async index(request, response) {
+    const {user_id} = request.query
+    const notes = await knex('notes').where({user_id}).orderBy("title")
+
+    return response.json(notes)
+  } 
 }
 
 module.exports = NotesController
