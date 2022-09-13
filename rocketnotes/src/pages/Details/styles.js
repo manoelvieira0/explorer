726e40e58154f9a81719bfef4f4ff0cc82f5776a
netovieira01_ <netinhovieira1801@gmail.com>
2022-09-13 11:42:14 -0300

    Children

--- rocketnotes/src/pages/Details/styles.js
+++ rocketnotes/src/pages/Details/styles.js
@@ -11,3 +11,15 @@ export const Container = styled.div`
   "content"
   ;
 `;
+
+export const Links = styled.ul`
+  list-style: none;
+
+  > li {
+    margin-top: 12px;
+  }
+
+  a {
+    color: ${({theme}) => theme.COLORS.WHITE};
+  }
+`
