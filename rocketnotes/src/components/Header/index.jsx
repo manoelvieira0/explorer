f0858b3c5f21982786c278d6d7b787c0f7bdfa9f
netovieira01_ <netinhovieira1801@gmail.com>
2022-09-13 11:22:00 -0300

    Imagem do usuário

--- rocketnotes/src/components/Header/index.jsx
+++ rocketnotes/src/components/Header/index.jsx
@@ -1,9 +1,15 @@
-import {Container} from './styles'
+import {Container, Profile} from './styles'
 
 export function Header(){
   return (
     <Container>
-
+      <Profile>
+        <img src="https://github.com/manoelvieira0.png" alt="GitHub Profile Picture" />
+        <div>
+          <span>Bem-Vindo,</span>
+          <strong>Manoel Vieira</strong>
+        </div>
+      </Profile>
     </Container>
   );
 }
