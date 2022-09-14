ce11da9fd329c73c3e78b7fb321b9e078d755718
netovieira01_ <netinhovieira1801@gmail.com>
2022-09-14 08:47:39 -0300

    Componente Tag

--- rocketnotes/src/pages/Details/index.jsx
+++ rocketnotes/src/pages/Details/index.jsx
@@ -3,6 +3,7 @@ import { Container, Links } from "./styles"
 import { Header } from "../../components/Header"
 import { Button } from "../../components/Button"
 import { Section } from "../../components/Section"
+import { Tag } from "../../components/Tag"
 
 export function Details() {
   return (
@@ -14,7 +15,13 @@ export function Details() {
           <li><a href="https://www.rocketseat.com.br/explorer">https://www.rocketseat.com.br/explorer</a></li>
         </Links>
       </Section>
-      <Button title="Voltar"/>
+
+      <Section title="Marcadores">
+        <Tag title="Express" />
+        <Tag title="Node" />
+      </Section>
+
+      <Button title="Voltar" />
     </Container>
   )
 }
