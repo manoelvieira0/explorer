6117f402c402fc06ed6a919ccd31d14bc4e2a284
netovieira01_ <netinhovieira1801@gmail.com>
2022-09-15 07:38:04 -0300

    Avatar do usuário e ajustes

--- rocketnotes/src/pages/Profile/index.jsx
+++ rocketnotes/src/pages/Profile/index.jsx
@@ -1,8 +1,8 @@
-import { Container, Form } from "./styles";
+import { Container, Form, Avatar } from "./styles";
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 
-import { FiArrowLeft, FiUser, FiLock, FiMail } from 'react-icons/fi'
+import { FiArrowLeft, FiUser, FiLock, FiMail, FiCamera } from 'react-icons/fi'
 
 export function Profile() {
   return (
@@ -14,12 +14,22 @@ export function Profile() {
       </header>
 
       <Form>
-        <Input placeholder="Nome" type="text" icon={FiUser}/>
-        <Input placeholder="E-mail" type="text" icon={FiMail}/>
-        <Input placeholder="Senha atual" type="password" icon={FiLock}/>
-        <Input placeholder="Nova senha" type="password" icon={FiLock}/>
+        <Avatar>
+          <img src="https://github.com/manoelvieira0.png" alt="Foto do usuário" />
 
-        <Button title="Salvar"/>
+          <label htmlFor="avatar">
+          <FiCamera/>
+
+          <input type="file" id="avatar" />
+          </label>
+        </Avatar>
+
+        <Input placeholder="Nome" type="text" icon={FiUser} />
+        <Input placeholder="E-mail" type="text" icon={FiMail} />
+        <Input placeholder="Senha atual" type="password" icon={FiLock} />
+        <Input placeholder="Nova senha" type="password" icon={FiLock} />
+
+        <Button title="Salvar" />
       </Form>
     </Container>
   )
