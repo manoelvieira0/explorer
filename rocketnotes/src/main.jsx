74dc62c7ca26d23974ee3be975407f8c9c918900
netovieira01_ <netinhovieira1801@gmail.com>
2022-09-15 06:15:10 -0300

    Estrutura da SignIn

--- rocketnotes/src/main.jsx
+++ rocketnotes/src/main.jsx
@@ -7,12 +7,13 @@ import theme from './styles/theme'
 
 import { Details } from './pages/Details'
 import { Home } from './pages/Home'
+import {SignIn} from './pages/SignIn'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>
 )
