ec1749e3d36e6b0823d499a22ece4036459ff3c7
netovieira01_ <netinhovieira1801@gmail.com>
2022-09-15 11:33:19 -0300

    Finalizando o New

--- rocketnotes/src/pages/New/index.jsx
+++ rocketnotes/src/pages/New/index.jsx
@@ -4,6 +4,7 @@ import { Input } from "../../components/Input";
 import { TextArea } from "../../components/TextArea";
 import { NoteItem } from "../../components/NoteItem";
 import { Section } from "../../components/Section";
+import { Button } from "../../components/Button";
 
 export function New() {
   return (
@@ -16,11 +17,20 @@ export function New() {
             <a href="/">Voltar</a>
           </header>
 
-          <Input placeholder="Título"/>
-          <TextArea placeholder="Observações"/>
-          <Section title="Links úteis"/>
-          <NoteItem value="https://rocketseat.com.br"/>
-          <NoteItem isNew placeholder="Novo link"/>
+          <Input placeholder="Título" />
+          <TextArea placeholder="Observações" />
+          <Section title="Links úteis">
+            <NoteItem value="https://rocketseat.com.br" />
+            <NoteItem isNew placeholder="Novo link" />
+          </Section>
+          <Section title="Marcadores">
+            <div className="tags">
+              <NoteItem value="React" />
+              <NoteItem isNew placeholder="Nova tag" />
+            </div>
+          </Section>
+
+          <Button title="Salvar"/>
         </Form>
       </main>
     </Container>
