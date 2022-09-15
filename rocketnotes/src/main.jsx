aabfab4893ba15e15d450877d4a4fe9460986cf4
netovieira01_ <netinhovieira1801@gmail.com>
2022-09-15 07:28:28 -0300

    Profile
    Formulários para cadastrar nome, e-mail e senha.

--- rocketnotes/src/main.jsx
+++ rocketnotes/src/main.jsx
@@ -9,12 +9,13 @@ import { Details } from './pages/Details'
 import { Home } from './pages/Home'
 import {SignIn} from './pages/SignIn'
 import {SignUp} from './pages/SignUp'
+import {Profile} from './pages/Profile'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignUp />
+      <Profile />
     </ThemeProvider>
   </React.StrictMode>
 )
---
+++ rocketnotes/src/pages/Profile/index.jsx
@@ -0,0 +1,26 @@
+import { Container, Form } from "./styles";
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+
+import { FiArrowLeft, FiUser, FiLock, FiMail } from 'react-icons/fi'
+
+export function Profile() {
+  return (
+    <Container>
+      <header>
+        <a href="/">
+          <FiArrowLeft />
+        </a>
+      </header>
+
+      <Form>
+        <Input placeholder="Nome" type="text" icon={FiUser}/>
+        <Input placeholder="E-mail" type="text" icon={FiMail}/>
+        <Input placeholder="Senha atual" type="password" icon={FiLock}/>
+        <Input placeholder="Nova senha" type="password" icon={FiLock}/>
+
+        <Button title="Salvar"/>
+      </Form>
+    </Container>
+  )
+}
