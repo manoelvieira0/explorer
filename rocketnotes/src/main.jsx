50f759cf34eb5bbc8d6bddd1e1f6b2a7d0d11e1f
netovieira01_ <netinhovieira1801@gmail.com>
2022-09-15 07:19:01 -0300

    SignUp

--- rocketnotes/src/main.jsx
+++ rocketnotes/src/main.jsx
@@ -8,12 +8,13 @@ import theme from './styles/theme'
 import { Details } from './pages/Details'
 import { Home } from './pages/Home'
 import {SignIn} from './pages/SignIn'
+import {SignUp} from './pages/SignUp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignIn />
+      <SignUp />
     </ThemeProvider>
   </React.StrictMode>
 )
---
+++ rocketnotes/src/pages/SignUp/index.jsx
@@ -0,0 +1,31 @@
+import { Container, Form, Background } from "./styles";
+import {Button} from '../../components/Button'
+import { Input } from '../../components/Input'
+
+import { FiMail, FiLock, FiUser } from 'react-icons/fi'
+
+export function SignUp() {
+  return (
+    <Container>
+      <Background />
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis</p>
+
+        <h2>Crie sua conta</h2>
+
+        <Input placeholder="Nome" type="text" icon={FiUser} />
+
+        <Input placeholder="E-mail" type="text" icon={FiMail} />
+
+        <Input placeholder="Senha" type="password" icon={FiLock} />
+
+        <Button title="Cadastrar"/>
+
+        <a href="#">
+          Criar Conta
+        </a>
+      </Form>
+    </Container>
+  )
+}
