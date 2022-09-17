4161c84458328ea090afcb79bec4af0a64fe42b9
netovieira01_ <netinhovieira1801@gmail.com>
2022-09-17 10:47:10 -0300

    Validando senha

--- api/src/controllers/SessionsController.js
+++ api/src/controllers/SessionsController.js
@@ -1,5 +1,6 @@
 const knex = require('../database/knex')
 const AppError = require('../utils/AppError')
+const { compare } = require("bcryptjs")
 
 class SessionsController {
   async create(request, response) {
@@ -7,7 +8,13 @@ class SessionsController {
 
     const user = await knex("users").where({ email }).first();
 
-    if(!user){
+    if (!user) {
+      throw new AppError("E-mail e/ou senha incorreta", 401)
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if(!passwordMatched){
       throw new AppError("E-mail e/ou senha incorreta", 401)
     }
 
