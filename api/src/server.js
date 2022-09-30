6766173aa0d76fceb8cd719f4287bdb529644756
netovieira01_ <netinhovieira1801@gmail.com>
2022-09-30 06:49:15 -0300

    método get

--- api/src/server.js
+++ api/src/server.js
@@ -2,3 +2,9 @@ const express = require("express")
 
 const app = express()
 
+app.get("/", (request, response) => {
+  response.send("Olá Mundo!")
+})
+
+const PORT = 3000
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
