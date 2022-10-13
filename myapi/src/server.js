c3438b5bd6240b8e51eff01b824dcf62b2b8a638
netovieira01_ <netinhovieira1801@gmail.com>
2022-10-13 15:16:15 -0300

    tratando exceções

--- myapi/src/server.js
+++ myapi/src/server.js
@@ -1,3 +1,7 @@
+require("express-async-errors")
+
+const AppError = require("./utils/AppError")
+
 const express = require('express')
 
 const routes = require("./routes")
@@ -6,6 +10,22 @@ const app = express()
 app.use(express.json())
 
 app.use(routes)
+
+app.use((error, request, response, next) => {
+  if(error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message
+    })
+  }
+
+  console.error(error)
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error'
+  })
+})
 
 const PORT = 3000
 app.listen(PORT, () => console.log(`O servidor está rodando na porta ${PORT}`))
