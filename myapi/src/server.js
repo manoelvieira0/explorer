cbefd7e3600102411f676daec8f0d199e5262ad3
netovieira01_ <netinhovieira1801@gmail.com>
2022-10-13 08:31:58 -0300

    Route params

--- myapi/src/server.js
+++ myapi/src/server.js
@@ -2,5 +2,10 @@ const express = require('express')
 
 const app = express()
 
+app.get("/message/:id/:user", (request, response) => {
+  const {id, user} = request.params
+  response.send(`Id da mensagem: ${id}. Para o usuário: ${user}`)
+})
+
 const PORT = 3000
 app.listen(PORT, () => console.log(`O servidor está rodando na porta ${PORT}`))
