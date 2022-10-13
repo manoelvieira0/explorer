219f2ea5deac8b1619443a645882604403c92aea
netovieira01_ <netinhovieira1801@gmail.com>
2022-10-13 08:37:01 -0300

    Query Params

--- myapi/src/server.js
+++ myapi/src/server.js
@@ -7,5 +7,11 @@ app.get("/message/:id/:user", (request, response) => {
   response.send(`Id da mensagem: ${id}. Para o usuário: ${user}`)
 })
 
+app.get("/users", (request, response) => {
+  const {page, limit} = request.query
+
+  response.send(`Página: ${page}, Limit: ${limit}`)
+})
+
 const PORT = 3000
 app.listen(PORT, () => console.log(`O servidor está rodando na porta ${PORT}`))
