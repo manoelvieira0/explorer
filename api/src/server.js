d99b61d6a2a582c5a5903e0ff29a644dfafe4710
netovieira01_ <netinhovieira1801@gmail.com>
2022-11-19 11:25:09 -0300

    Método get, enviando resposta

--- api/src/server.js
+++ api/src/server.js
@@ -2,5 +2,9 @@ const express = require("express") // Importando o express
 
 const app = express() // Inicializando o express
 
+app.get("/", (request, response) => {
+  response.send("Hello")
+})
+
 const PORT = 3000 // Constante com o endereço da porta
 app.listen(PORT, () => console.log(`O servidor está rodando na porta ${PORT}`)) 
