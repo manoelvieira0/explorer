22fb92dff12381ca360279ccdf3a32192b5f2e5e
netovieira01_ <netinhovieira1801@gmail.com>
2022-11-19 11:32:25 -0300

    route params

--- api/src/server.js
+++ api/src/server.js
@@ -2,8 +2,9 @@ const express = require("express") // Importando o express
 
 const app = express() // Inicializando o express
 
-app.get("/", (request, response) => {
-  response.send("Hello")
+app.get("/users/:id/:user", (request, response) => {
+  const {id, user} = request.params
+  response.send(`Id da mensagem: ${id}, para o usuário: ${user}`)
 })
 
 const PORT = 3000 // Constante com o endereço da porta
