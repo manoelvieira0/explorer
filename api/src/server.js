ca2174fcb55062a2b24f19555a15d84219b8d317
netovieira01_ <netinhovieira1801@gmail.com>
2022-11-19 11:04:38 -0300

    facilitando com script de execução

--- api/src/server.js
+++ api/src/server.js
@@ -2,6 +2,5 @@ const express = require("express") // Importando o express
 
 const app = express() // Inicializando o express
 
-const PORT = 3000
-
-app.listen(PORT, () => console.log(`O servidor está rodando na porta ${PORT}`))
+const PORT = 3000 // Constante com o endereço da porta
+app.listen(PORT, () => console.log(`O servidor está rodando na porta ${PORT}`)) 
