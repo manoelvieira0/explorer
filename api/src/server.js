02bc3bc1fae5c030714912e1d0e81f12d91c340d
netovieira01_ <netinhovieira1801@gmail.com>
2022-11-19 11:42:50 -0300

    Query Params

--- api/src/server.js
+++ api/src/server.js
@@ -2,9 +2,14 @@ const express = require("express") // Importando o express
 
 const app = express() // Inicializando o express
 
-app.get("/users/:id/:user", (request, response) => {
-  const {id, user} = request.params
+app.get("/users/:id/:user", (request, response) => { // Route Params
+  const { id, user } = request.params
   response.send(`Id da mensagem: ${id}, para o usuário: ${user}`)
+})
+
+app.get("/users", (request, response) => { // Query Params
+  const { page, limit } = request.query
+  response.send(`Página: ${page}. Mostrar: ${limit}`)
 })
 
 const PORT = 3000 // Constante com o endereço da porta
