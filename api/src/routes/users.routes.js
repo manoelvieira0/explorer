5b1c6cc48c0a18000f3006e1c180b370ed17b43c
netovieira01_ <netinhovieira1801@gmail.com>
2022-11-21 19:42:17 -0300

    Utilizando Middlewares
    Interceptadores

--- api/src/routes/users.routes.js
+++ api/src/routes/users.routes.js
@@ -4,8 +4,17 @@ const usersRoutes = Router()
 
 const UsersController = require("../controllers/UsersController")
 
+function myMiddleware(request, response, next) {
+
+  if (!request.body.isAdmin) {
+    return response.json({ message: 'User Unathorized' })
+  }
+
+  next()
+}
+
 const usersController = new UsersController()
 
-usersRoutes.post("/", usersController.create)
+usersRoutes.post("/", myMiddleware, usersController.create)
 
 module.exports = usersRoutes
