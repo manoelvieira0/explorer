c886b1f882a2efa9895ee12c5f44c0496ffc2b2f
netovieira01_ <netinhovieira1801@gmail.com>
2022-11-21 19:20:39 -0300

    HTTP Codes

--- api/src/controllers/UsersController.js
+++ api/src/controllers/UsersController.js
@@ -1,7 +1,7 @@
 class UsersController {
   create(request, response) {
     const { name, email, password } = request.body
-    response.json({ name, email, password })
+    response.status(201).json({ name, email, password })
   }
 }
 
