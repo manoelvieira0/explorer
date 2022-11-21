94ce57467426522562f9b69ea2fa9f10244643c3
netovieira01_ <netinhovieira1801@gmail.com>
2022-11-21 20:00:29 -0300

    Criando classe de erro. AppError

--- api/src/routes/users.routes.js
+++ api/src/routes/users.routes.js
@@ -4,17 +4,8 @@ const usersRoutes = Router()
 
 const UsersController = require("../controllers/UsersController")
 
-function myMiddleware(request, response, next) {
-
-  if (!request.body.isAdmin) {
-    return response.json({ message: 'User Unathorized' })
-  }
-
-  next()
-}
-
 const usersController = new UsersController()
 
-usersRoutes.post("/", myMiddleware, usersController.create)
+usersRoutes.post("/", usersController.create)
 
 module.exports = usersRoutes
