f1a1aa7fe4396732392bada4d2141a112386a648
netovieira01_ <netinhovieira1801@gmail.com>
2022-11-21 08:26:33 -0300

    Body Params

--- api/src/server.js
+++ api/src/server.js
@@ -1,10 +1,11 @@
 const express = require("express") // Importando o express
 
 const app = express() // Inicializando o express
+app.use(express.json()) // Informar que utilizaremos o formato JSON
 
-app.post("/users", (request, response) => { // Query Params
-
-  response.send("Você chamou o POST")
+app.post("/users", (request, response) => { // Body Params
+  const {name, email, password} = request.body
+  response.json({name, email, password}) // Utilizando a resposta como JSON
 })
 
 const PORT = 3000 // Constante com o endereço da porta
