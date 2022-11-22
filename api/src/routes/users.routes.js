59e4574ed54711a5776ac9fde3e7a53636c89846
netovieira01_ <netinhovieira1801@gmail.com>
2022-11-22 13:33:41 -0300

    Atualizando usuário
    dados dos usuários sejam atualizados.

--- api/src/routes/users.routes.js
+++ api/src/routes/users.routes.js
@@ -7,5 +7,6 @@ const UsersController = require("../controllers/UsersController")
 const usersController = new UsersController()
 
 usersRoutes.post("/", usersController.create)
+usersRoutes.put("/:id", usersController.update)
 
 module.exports = usersRoutes
