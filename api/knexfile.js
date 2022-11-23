082ccd8bd36058fb94d4320ce3df4f4a3f5e1bdf
netovieira01_ <netinhovieira1801@gmail.com>
2022-11-23 11:06:29 -0300

    Migration para notes
    Comando para criar uma tabelas inseridas automaticamente:
    npx knex migrate:make createNotes
    
    para executar migrations:
    npx knex migrate:latest

--- api/knexfile.js
+++ api/knexfile.js
@@ -6,6 +6,9 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
     },
-    useNullAsDefault: true
+    useNullAsDefault: true,
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+    }
   },
 };
