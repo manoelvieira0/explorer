da18eeff3c7a1501ac3520b52d91f7bb57142565
netovieira01_ <netinhovieira1801@gmail.com>
2022-11-23 09:22:17 -0300

    Atualizando senha

--- api/src/controllers/UsersController.js
+++ api/src/controllers/UsersController.js
@@ -1,6 +1,6 @@
 const AppError = require('../utils/AppError')
 
-const { hash } = require('bcryptjs')
+const { hash, compare } = require('bcryptjs')
 
 const sqliteConnection = require('../database/sqlite')
 
@@ -23,7 +23,7 @@ class UsersController {
   }
 
   async update(request, response) {
-    const { name, email } = request.body
+    const { name, email, password, old_password } = request.body
     const { id } = request.params
 
     const database = await sqliteConnection()
@@ -42,13 +42,28 @@ class UsersController {
     user.name = name;
     user.email = email;
 
+    if(password && !old_password){
+      throw new AppError("Você precisa informar a senha antiga para renovar a senha.")
+    }
+
+    if(password && old_password){
+      const checkOldPassword = await compare(old_password, user.password)
+
+      if(!checkOldPassword){
+        throw new AppError("A senha antiga não confere")
+      }
+
+      user.password = await hash(password, 8)
+    }
+
     await database.run(`
     UPDATE users SET
     name = ?,
     email = ?,
+    password = ?,
     updated_at = ?
     WHERE id = ?`,
-      [user.name, user.email, new Date(), id]
+      [user.name, user.email, user.password, new Date(), id]
     );
 
     return response.json()
