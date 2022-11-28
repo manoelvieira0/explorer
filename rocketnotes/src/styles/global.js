756e87da66d1fe05a8614f40318f5f26ec65a9d1
netovieira01_ <netinhovieira1801@gmail.com>
2022-11-28 20:56:25 -0300

    Fontes personalizados

--- rocketnotes/src/styles/global.js
+++ rocketnotes/src/styles/global.js
@@ -9,7 +9,14 @@ export default createGlobalStyle`
 
   body {
     background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
-    color: ${({ theme }) => theme.COLORS.WHITE};;
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    -webkit-font-smoothing: antialiased; // Para deixar fontes mais suavizadas
+  }
+
+  body, input, button, textarea {
+    font-family: 'Roboto Slab', serif;
+    font-size: 16px;
+    outline: none;
   }
 
   a {
