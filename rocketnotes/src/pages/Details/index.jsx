b4e510ed15fd41f99c7ae826025aed95b15e8f4c
netovieira01_ <netinhovieira1801@gmail.com>
2022-11-29 09:22:56 -0300

    Componentes - como criar

--- rocketnotes/src/pages/Details/index.jsx
+++ rocketnotes/src/pages/Details/index.jsx
@@ -1,10 +1,12 @@
 import { Container } from './styles'
+import { Button } from '../../components/Button'
 
 export function Details() {
   return (
     <Container>
       <h1>Olá mundo!</h1>
       <span>Manoel Vieira</span>
+      <Button />
     </Container>
   )
 }
