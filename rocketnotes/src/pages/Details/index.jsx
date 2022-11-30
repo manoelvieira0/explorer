1b1ef282eadfd0a2d2d1766f1ea87f49ee6c033c
netovieira01_ <netinhovieira1801@gmail.com>
2022-11-30 11:27:46 -0300

    Finalizando página de detalhes

--- rocketnotes/src/pages/Details/index.jsx
+++ rocketnotes/src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from './styles'
+import { Container, Links, Content } from './styles'
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
 import { Section } from '../../components/Section'
@@ -9,22 +9,32 @@ export function Details() {
   return (
     <Container>
       <Header />
+      <main>
+        <Content>
+          <ButtonText title="Excluir Nota" />
 
-      <ButtonText title="Excluir Nota"/>
+          <h1>
+            Introdução ao React
+          </h1>
 
-      <Section title="Links úteis">
-        <Links>
-          <li><a href="#">https://www.rocketseat.com.br/explorer</a></li>
-          <li><a href="#">https://www.rocketseat.com.br/explorer</a></li>
-        </Links>
-      </Section>
+          <p>O React é uma biblioteca front-end e tem como um de seus objetivos facilitar a conexão entre diferentes partes de uma página, portanto seu funcionamento acontece através do que chamamos de componentes.</p>
 
-      <Section title="Marcadores">
-        <Tag title="Express" />
-        <Tag title="Node" />
-      </Section>
+          <Section title="Links úteis">
+            <Links>
+              <li><a href="#">https://www.rocketseat.com.br/explorer</a></li>
+              <li><a href="#">https://www.rocketseat.com.br/explorer</a></li>
+            </Links>
+          </Section>
 
-      <Button title="Voltar" />
+          <Section title="Marcadores">
+            <Tag title="Express" />
+            <Tag title="Node" />
+          </Section>
+
+          <Button title="Voltar" />
+
+        </Content>
+      </main>
     </Container>
   )
 }
--- rocketnotes/src/pages/Details/styles.js
+++ rocketnotes/src/pages/Details/styles.js
@@ -10,6 +10,11 @@ export const Container = styled.div`
   "header"
   "content"
   ; // Para nomear as regiões
+   > main {
+    grid-area: content; // Conteúdo
+    overflow-y: scroll;
+    padding: 64px 0;
+   }
 `;
 
 export const Links = styled.ul`
@@ -21,5 +26,31 @@ export const Links = styled.ul`
 
   a {
     color: ${({theme}) => theme.COLORS.WHITE};
+  }
+`;
+
+export const Content = styled.div`
+  max-width: 600px;
+  margin: 0 auto; // Para levar todo conteúdo para o centro da tela
+
+  display: flex;
+  flex-direction: column;
+
+  > button:first-child {
+    align-self: end;
+  }
+
+  > h1 {
+    font-size: 36px;
+    font-weight: 500;
+
+    padding-top: 54px;
+  }
+
+  > p {
+    font-size: 16px;
+    margin-top: 16px;
+
+    text-align: justify;
   }
 `;
