f893b3f8083dbff1f328596fc96993f886674b5f
netovieira01_ <netinhovieira1801@gmail.com>
2022-12-01 15:05:22 -0300

    Interface New

---
+++ rocketnotes/src/pages/New/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./styles";
+
+import { Header } from '../../components/Header'
+
+export function New(){
+  return (
+    <Container>
+      <Header/>
+    </Container>
+  )
+}
