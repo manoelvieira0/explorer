957a2cea8dcdff9f3994d98ae379bdedb6e26a7e
netovieira01_ <netinhovieira1801@gmail.com>
2022-12-01 10:00:31 -0300

    Formulário da SignIn

--- rocketnotes/src/pages/SignIn/index.jsx
+++ rocketnotes/src/pages/SignIn/index.jsx
@@ -1,9 +1,29 @@
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
+import { Input } from '../../components/Input'
+import { FiMail, FiLock } from 'react-icons/fi'
+import { Button } from '../../components/Button'
 
-export function SignIn(){
-  return(
+export function SignIn() {
+  return (
     <Container>
-      
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+        <h2>Faça seu login</h2>
+        <Input
+          placeholder="E-mail"
+          type="email"
+          icon={FiMail}
+        />
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+        />
+        <Button title="Entrar"/>
+
+        <a href="#">Criar conta</a>
+      </Form>
     </Container>
   );
 }
