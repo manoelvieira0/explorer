6ff05b0372312e3274866f93c108105d0486b09a
netovieira01_ <netinhovieira1801@gmail.com>
2022-12-01 15:01:34 -0300

    Avatar do usuário e finalização da tela de usuário

--- rocketnotes/src/pages/Profile/index.jsx
+++ rocketnotes/src/pages/Profile/index.jsx
@@ -1,8 +1,8 @@
-import { Container, Form } from "./styles";
+import { Container, Form, Avatar } from "./styles";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 
-import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi'
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
 
 export function Profile() {
   return (
@@ -13,6 +13,14 @@ export function Profile() {
         </a>
       </header>
       <Form>
+        <Avatar>
+          <img src="https://github.com/manoelvieira0.png" alt="Foto do usuário" />
+          <label htmlFor="avatar">
+            <FiCamera/>
+            <input id="avatar" type="file"/>
+          </label>
+        </Avatar>
+
         <Input placeholder="Nome" type="text" icon={FiUser}/>
         <Input placeholder="E-mail" type="email" icon={FiMail}/>
         <Input placeholder="Senha atual" type="password" icon={FiLock}/>
--- rocketnotes/src/pages/Profile/styles.js
+++ rocketnotes/src/pages/Profile/styles.js
@@ -6,14 +6,14 @@ export const Container = styled.div`
   > header {
     width: 100%;
     height: 144px;
-    background: ${({theme}) => theme.COLORS.BACKGROUND_900};
+    background: ${({ theme }) => theme.COLORS.BACKGROUND_900};
 
     display: flex;
     align-items: center;
     padding: 0 124px;
   
     svg {
-      color: ${({theme}) => theme.COLORS.GRAY_100};
+      color: ${({ theme }) => theme.COLORS.GRAY_100};
       font-size: 24px;
     }
   }
@@ -22,4 +22,51 @@ export const Container = styled.div`
 export const Form = styled.form`
   max-width: 400px;
   margin: 30px auto 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  > div:nth-child(4){
+    margin-top: 24px;
+  }
+`;
+
+export const Avatar = styled.div`
+  position: relative;
+  margin: -100px auto 32px;
+
+  > img {
+    width: 186px;
+    height: 186px;
+    border-radius: 50%;
+  }
+
+  > label {
+    width: 48px;
+    height: 48px;
+
+    background-color: ${({ theme }) => theme.COLORS.ORANGE};
+    border-radius: 50%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    position: absolute;
+    bottom: 7px;
+    right: 7px;
+
+    cursor: pointer;
+
+    > input {
+      display: none;
+    }
+
+    svg {
+      width: 20px;
+      height: 20px;
+    }
+  }
 `;
