bbd9c1f1ecc20741d89cd180ae92e73d0c33ad7b
netovieira01_ <netinhovieira1801@gmail.com>
2022-12-01 13:40:06 -0300

    SignUp

---
+++ rocketnotes/src/pages/SignUp/index.jsx
@@ -0,0 +1,35 @@
+import { Container, Form, Background } from "./styles";
+import { Input } from '../../components/Input'
+import { FiMail, FiLock, FiUser } from 'react-icons/fi'
+import { Button } from '../../components/Button'
+
+export function SignUp() {
+  return (
+    <Container>
+      <Background />
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+        <h2>Crie sua conta</h2>
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+        />
+        <Input
+          placeholder="E-mail"
+          type="email"
+          icon={FiMail}
+        />
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+        />
+        <Button title="Cadastrar" />
+
+        <a href="#">Voltar para o login</a>
+      </Form>
+    </Container>
+  );
+}
