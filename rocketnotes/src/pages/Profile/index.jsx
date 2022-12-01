83879c3428eb1c11f8b9da070257b01c7b26e5a2
netovieira01_ <netinhovieira1801@gmail.com>
2022-12-01 14:49:06 -0300

    Profile

---
+++ rocketnotes/src/pages/Profile/index.jsx
@@ -0,0 +1,25 @@
+import { Container, Form } from "./styles";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi'
+
+export function Profile() {
+  return (
+    <Container>
+      <header>
+        <a href="/">
+          <FiArrowLeft />
+        </a>
+      </header>
+      <Form>
+        <Input placeholder="Nome" type="text" icon={FiUser}/>
+        <Input placeholder="E-mail" type="email" icon={FiMail}/>
+        <Input placeholder="Senha atual" type="password" icon={FiLock}/>
+        <Input placeholder="Nova senha" type="password" icon={FiLock}/>
+
+        <Button title="Salvar"/>
+      </Form>
+    </Container>
+  );
+}
