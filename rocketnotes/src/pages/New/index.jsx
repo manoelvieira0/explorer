42325d19c93c251e47e4aa9d785e0fe60a6c9691
netovieira01_ <netinhovieira1801@gmail.com>
2022-12-01 15:10:30 -0300

    Header da New

--- rocketnotes/src/pages/New/index.jsx
+++ rocketnotes/src/pages/New/index.jsx
@@ -1,11 +1,21 @@
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
 
 import { Header } from '../../components/Header'
+import { Input } from "../../components/Input";
 
-export function New(){
+export function New() {
   return (
     <Container>
-      <Header/>
+      <Header />
+      <main>
+        <Form>
+          <header>
+            <h1>Criar nota</h1>
+            <a href="/">Voltar</a>
+          </header>
+          <Input placeholder="Título"/>
+        </Form>
+      </main>
     </Container>
   )
 }
