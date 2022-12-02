d3013d25bfbbd7090b7d962158acf619cabcf78d
netovieira01_ <netinhovieira1801@gmail.com>
2022-12-01 22:01:09 -0300

    Rotas da aplicação

--- rocketnotes/src/main.jsx
+++ rocketnotes/src/main.jsx
@@ -5,13 +5,13 @@ import GlobalStyle from './styles/global';
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 
-import { New } from './pages/New'
+import { Routes } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <New />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>
 )
