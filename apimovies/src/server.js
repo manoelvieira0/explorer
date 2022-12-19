889bd49d2e9e55b81af34c8d6d46331969d5e88b
netovieira01_ <netinhovieira1801@gmail.com>
2022-12-19 17:51:49 -0300

    Body params

--- apimovies/src/server.js
+++ apimovies/src/server.js
@@ -2,8 +2,12 @@ const express = require('express') // Importando o Express
 
 const app = express() // Inicializando o Express
 
+app.use(express.json()) // Para informar que será utilizado o formato JSON
+
 app.post("/users", (request, response) => { // POST
-  response.send("Você chamou o POST")
+  const {name, email, password} = request.body // Para pegar informações do corpo da requisição
+
+  response.json({name, email, password}) // Utilizando o padrão JSON para obter
 })
 
 const PORT = 3333 // Endereço do Servidor
