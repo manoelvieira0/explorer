bba02c7e1de96a6ea437c21e99cde825f9544d9c
netovieira01_ <netinhovieira1801@gmail.com>
2022-12-19 17:23:36 -0300

    Instalando o nodemon
     npm install nodemon --save-dev

--- apimovies/src/server.js
+++ apimovies/src/server.js
@@ -2,5 +2,5 @@ const express = require('express') // Importando o Express
 
 const app = express() // Inicializando o Express
 
-const PORT = 3333 // Endereço
+const PORT = 3333 // Endereço do Servidor
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)) // Para escutar
