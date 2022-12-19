893caf082d37c20d8e4d268ad2d729481ac77866
netovieira01_ <netinhovieira1801@gmail.com>
2022-12-19 17:28:45 -0300

    Método Get

--- apimovies/src/server.js
+++ apimovies/src/server.js
@@ -2,5 +2,9 @@ const express = require('express') // Importando o Express
 
 const app = express() // Inicializando o Express
 
+app.get("/", (request, response) => { 
+  response.send("Hello Movies!")
+})
+
 const PORT = 3333 // Endereço do Servidor
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)) // Para escutar
