612fd1ad2c287da5ae48bc1bc7e9b3bf5e928814
netovieira01_ <netinhovieira1801@gmail.com>
2022-12-19 20:03:23 -0300

    Tratando exceções
    npm install express-async-errors --save
    Biblioteca para erros

--- apimovies/src/controllers/usersController.js
+++ apimovies/src/controllers/usersController.js
@@ -1,6 +1,12 @@
+const AppError = require('../utils/AppError')
+
 class UsersController {
   create(request, response) {
     const { name, email, password } = request.body // Para pegar informações do corpo da requisição
+
+    if(!name){
+      throw new AppError("O nome é obrigatório.") // Para informar o erro
+    }
 
     response.status(201).json({ name, email, password }) // Utilizando o padrão JSON para obter
   }
--- apimovies/src/server.js
+++ apimovies/src/server.js
@@ -1,3 +1,5 @@
+require('express-async-errors') // Importando para separar erros do cliente e do servidor
+const AppError = require("./utils/AppError")
 const express = require('express') // Importando o Express
 const routes = require('./routes') // Importando as rotas em geral
 
@@ -5,6 +7,22 @@ const app = express() // Inicializando o Express
 app.use(express.json()) // Para informar que será utilizado o formato JSON
 
 app.use(routes)
+
+app.use((error, request, response, next) => {
+  if(error instanceof AppError){ // error instanciado por AppError
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    })
+  } // Erro do cliente
+
+  console.log(error)
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal Server Error"
+  })
+})
 
 const PORT = 3333 // Endereço do Servidor
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)) // Para escutar
