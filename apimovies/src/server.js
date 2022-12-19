937b3287c279fe32b0b947b24aaff3f67486e62a
netovieira01_ <netinhovieira1801@gmail.com>
2022-12-19 17:42:21 -0300

    Query Params

--- apimovies/src/server.js
+++ apimovies/src/server.js
@@ -2,9 +2,14 @@ const express = require('express') // Importando o Express
 
 const app = express() // Inicializando o Express
 
-app.get("/users/:id/:user", (request, response) => {
+app.get("/users/:id/:user", (request, response) => { // Route Params
   const { id, user } = request.params
   response.send(`Id da mensagem: ${id}, Para usuário ${user}`)
+})
+
+app.get("/users", (request, response) => { // Query Params
+  const { page, limit } = request.query
+  response.send(`Página: ${page}, Limit: ${limit}`)
 })
 
 const PORT = 3333 // Endereço do Servidor
