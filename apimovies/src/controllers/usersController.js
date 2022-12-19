305af0031d059ec981d705bf8bc3c9c233210168
netovieira01_ <netinhovieira1801@gmail.com>
2022-12-19 19:32:38 -0300

    Criando Controllers

---
+++ apimovies/src/controllers/usersController.js
@@ -0,0 +1,11 @@
+class UsersController {
+  /* 
+    Index - GET para listar vários registros
+    Show - GET para exibir registro específico
+    Create - POST para criar um registro
+    Update - PUT para atualizar um registro
+    Delete - DELETE para remover um registro
+  */
+}
+
+module.exports = UsersController
