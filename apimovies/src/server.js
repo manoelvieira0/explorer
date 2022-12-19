516ecc753820ce368233afbc7facc2d77a0499ca
netovieira01_ <netinhovieira1801@gmail.com>
2022-12-19 17:36:58 -0300

    Route params

--- apimovies/src/server.js
+++ apimovies/src/server.js
@@ -2,8 +2,9 @@ const express = require('express') // Importando o Express
 
 const app = express() // Inicializando o Express
 
-app.get("/", (request, response) => { 
-  response.send("Hello Movies!")
+app.get("/users/:id/:user", (request, response) => {
+  const { id, user } = request.params
+  response.send(`Id da mensagem: ${id}, Para usuário ${user}`)
 })
 
 const PORT = 3333 // Endereço do Servidor
