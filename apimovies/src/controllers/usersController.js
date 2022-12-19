015c1e02142d651d70deca6ac82341df3f2453b1
netovieira01_ <netinhovieira1801@gmail.com>
2022-12-19 19:54:27 -0300

    Criação da classe App Error

--- apimovies/src/controllers/usersController.js
+++ apimovies/src/controllers/usersController.js
@@ -1,12 +1,4 @@
 class UsersController {
-  /* 
-    Index - GET para listar vários registros
-    Show - GET para exibir registro específico
-    Create - POST para criar um registro
-    Update - PUT para atualizar um registro
-    Delete - DELETE para remover um registro
-  */
-
   create(request, response) {
     const { name, email, password } = request.body // Para pegar informações do corpo da requisição
 
