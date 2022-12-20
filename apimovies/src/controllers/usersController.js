eab3966a97f6c121009084a77054d37b43f28083
netovieira01_ <netinhovieira1801@gmail.com>
2022-12-20 16:05:38 -0300

    atualizando usuário - função update

--- apimovies/src/controllers/usersController.js
+++ apimovies/src/controllers/usersController.js
@@ -20,6 +20,36 @@ class UsersController {
     await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword]) // Para inserir na tabela 
     return response.status(201).json()
   }
+
+  async update(request, response) {
+    const { name, email } = request.body
+    const { id } = request.params
+
+    const database = await sqliteConnection()
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]) // Obtendo o user de acordo com o id
+
+    if(!user){
+      throw new AppError("Usuário não encontrado.")
+    }
+
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]) // Para verificar se a pessoa está mudando seu e-mail para outro e-mail que já exista
+
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id){ 
+      throw new AppError("Este e-mail já está em uso.")
+    }
+
+    user.name = name
+    user.email = email
+
+    await database.run(`
+    UPDATE users SET
+    name = ?,
+    email = ?,
+    updated_at = ?
+    WHERE id = ?
+    `, [user.name, user.email, new Date(), id]);
+    return response.json()
+  }
 }
 
 module.exports = UsersController
--- apimovies/src/routes/users.routes.js
+++ apimovies/src/routes/users.routes.js
@@ -7,5 +7,6 @@ const usersRoutes = Router() // Criando a rota dos usuários
 const usersController = new UsersController() // Instanciando 
 
 usersRoutes.post("/", usersController.create) // Pssando a responsabilidade para o método create dentro do usersController
+usersRoutes.put("/:id", usersController.update)
 
 module.exports = usersRoutes // Exportando
