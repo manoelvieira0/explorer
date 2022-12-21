0b3c58cfa21b5d0f46d5a5d1ebc1edccddf98334
netovieira01_ <netinhovieira1801@gmail.com>
2022-12-21 15:27:45 -0300

    Obtendo Tags da nota

--- apimovies/src/controllers/NotesController.js
+++ apimovies/src/controllers/NotesController.js
@@ -50,7 +50,17 @@ class NotesController {
 
     if (tags) {
       const filterTags = tags.split(',').map(tag => tag.trim()); // Para filtrar as tags
-      notes = await knex("tags").whereIn("name", filterTags);
+      notes = await knex("tags")
+      .select([
+        "notes.id",
+        "notes.title",
+        "notes.user_id"
+      ])
+      .where("notes.user_id", user_id)
+      .whereLike("notes.title", `%${title}%`)
+      .whereIn("name", filterTags)
+      .innerJoin("notes", "notes.id", "tags.note_id")
+      .orderBy("notes.title")
 
     } else {
       notes = await knex("notes")
@@ -58,7 +68,18 @@ class NotesController {
         .whereLike("title", `%${title}%`)
         .orderBy("title")
     }
-    return response.json(notes)
+
+    const userTags = await knex("tags").where({user_id})
+    const notesWithTags = notes.map(note => {
+      const noteTags = userTags.filter(tag => tag.note_id === note.id)
+
+      return {
+        ...note,
+        tags: noteTags
+      }
+    })
+
+    return response.json(notesWithTags)
   }
 }
 
