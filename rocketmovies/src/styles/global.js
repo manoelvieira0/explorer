66e30faf26ba118442d727b79c0a79690885dc52
netovieira01_ <netinhovieira1801@gmail.com>
2022-12-22 10:43:01 -0300

    Estilo Global

---
+++ rocketmovies/src/styles/global.js
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle` // Para utilizar o style global
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box; // Evitar que quando o padding interno influencia no tamanho do elemento
+  }
+
+  body {
+    background-color: ${({theme}) => theme.COLORS.BACKGROUND_800};
+    color: ${({theme}) => theme.COLORS.WHITE};
+    
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button, a {
+    cursor: pointer;
+    transition: filter 0.2s;
+  }
+
+  button:hover, a:hover {
+    filter: brightness(0.9);
+  }
+
+`;
