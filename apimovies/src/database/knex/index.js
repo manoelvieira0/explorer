bcde342b65c444646dc51f53443291423115e6e5
netovieira01_ <netinhovieira1801@gmail.com>
2022-12-22 15:58:02 -0300

    exclui

--- apimovies/src/database/knex/index.js
+++
@@ -1,6 +0,0 @@
-const config = require("../../../knexfile")
-const knex = require("knex")
-
-const connection = knex(config.development) // Conexão com do knex com o banco de dados
-
-module.exports = connection
--- rocketnotes/src/components/ButtonText/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Container } from "./styles";
-
-export function ButtonText({title, isActive = false, ...rest}){
-  return(
-    <Container type="button" isActive={isActive} {...rest}>
-      {title}
-    </Container>
-  )
-}
