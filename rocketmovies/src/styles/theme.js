10d7233860aca6171aa4b08563d1f5bc3180371e
netovieira01_ <netinhovieira1801@gmail.com>
2022-12-22 10:31:58 -0300

    Tema da aplicação

---
+++ rocketmovies/src/styles/theme.js
@@ -0,0 +1,13 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: "#1C1B1E",
+    BACKGROUND_800: "#262529",
+    BACKGROUND_700: "#948F99",
+
+    WHITE: "#F4EDE8",
+    PINK: "#FF859B",
+
+    GRAY_100: "#999591",
+    GRAY_300: "#666360",
+  }
+}
