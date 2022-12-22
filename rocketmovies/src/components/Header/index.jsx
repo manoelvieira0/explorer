421b576d41f7acd7bae7c284088affd0d0716598
netovieira01_ <netinhovieira1801@gmail.com>
2022-12-22 14:22:25 -0300

    Header

--- rocketmovies/src/components/Header/index.jsx
+++ rocketmovies/src/components/Header/index.jsx
@@ -1,9 +1,16 @@
-import { Container } from "./styles";
+import { Container, Profile } from "./styles";
 
-export function Header(){
+export function Header() {
   return (
     <Container>
-      
+      <h1>RocketMovies</h1>
+      <Profile>
+        <div>
+          <strong>Manoel Vieira</strong>
+          <span>Sair</span>
+        </div>
+        <img src="https://github.com/manoelvieira0.png" alt="Foto do usuário" />
+      </Profile>
     </Container>
   )
 }
--- rocketmovies/src/components/Header/styles.js
+++ rocketmovies/src/components/Header/styles.js
@@ -8,12 +8,45 @@ export const Container = styled.header`
 
   border-bottom-width: 1px;
   border-bottom-style: solid;
-  border-bottom-color: ${({theme}) => theme.COLORS.GRAY_300};
+  border-bottom-color: ${({ theme }) => theme.COLORS.GRAY_300};
 
   display: flex;
   justify-content: space-between;
+  align-items: center;
 
-  padding: 0 80px;
+  padding: 0 125px;
 
-  background: red;
+  > h1 {
+    color: ${({ theme }) => theme.COLORS.PINK};
+    font-size: 24px;
+  }
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+
+  > img {
+    width: 56px;
+    height: 56px;
+
+    border-radius: 50%;
+  }
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    margin-right: 20px;
+    line-height: 24px;
+
+    strong {
+      font-size: 18px;
+      color: ${({ theme }) => theme.COLORS.WHITE};
+    }
+
+    span {
+      font-size: 14px;
+      color: ${({ theme }) => theme.COLORS.GRAY_100};
+    }
+  }
 `;
